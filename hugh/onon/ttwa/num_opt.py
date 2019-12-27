e830bbc9dc3c1b1d71f53d45ef9f6f58db99268b
hughChen <shuliyuanshi@163.com>
2019-12-27 22:15:14 +0800

    【add】n个数的前k个小数

--- hugh/onon/ttwa/num_opt.py
+++ hugh/onon/ttwa/num_opt.py
@@ -52,6 +52,53 @@ def valid_perfect_num(n):
 
 
 
+def swap_data(num1, num2):
+    '''
+    交换两个数
+    :param num1:
+    :param num2:
+    :return:
+    '''
+    tmp = num1
+    num1 = num2
+    num2 = tmp
+    return num1, num2
+
+
+def arange_right(data, left, right, k):
+    '''
+    获取前k个最小的数
+    :param data:
+    :param right:
+    :param left:
+    :param k:
+    :return:
+    '''
+    if left >= right:
+        return data
+
+    i,j = left, right
+    tmp = data[j]
+    while i != j:
+        while i < j and data[i] <= tmp:
+            i += 1
+        data[i], data[j] = swap_data(data[i], data[j])
+        while i < j and data[j] > tmp:
+            j -= 1
+        data[i], data[j] = swap_data(data[i], data[j])
+
+    tmp = 1 if data[i] <= tmp else 0
+    # print(data)
+    if i == right:
+        return arange_right(data, left, right - 1, k)
+    elif i - left + tmp == k:
+        return data
+    elif i - left + tmp < k:
+        return arange_right(data, i + tmp, right, k - i - tmp + left)
+    else:
+        return arange_right(data, left, i + tmp, k)
+
+
 if __name__ == '__main__':
     print('-----')
     # s = '47Aa'
@@ -96,10 +143,20 @@ if __name__ == '__main__':
     #     else:
     #         r_mes += r
     # print(r_mes)
-    count = 0
-    for i in range(2, 2670):
-        if valid_perfect_num(i):
-            count += 1
-        print(i, valid_perfect_num(i), count)
-    print(count)
-    print(fib(9))
+
+    # count = 0
+    # for i in range(2, 2670):
+    #     if valid_perfect_num(i):
+    #         count += 1
+    #     print(i, valid_perfect_num(i), count)
+    # print(count)
+    # print(fib(9))
+
+    line = input().strip()
+    num, n = [int(i) for i in line.split(' ')]
+    data = list(map(int, input().strip().split(' ')))
+    print(len(data))
+    n_data = arange_right(data, 0, num - 1, n)
+    n_data = n_data[0:n]
+    n_data = sorted(n_data)
+    print(' '.join([str(j) for j in n_data]))
