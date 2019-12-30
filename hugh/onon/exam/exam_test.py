c185c22c54a0f48f30b8c558f59636e8ad2f7b8f
hughChen <shuliyuanshi@163.com>
2019-12-30 11:09:35 +0800

    【add】N个桃树Ni个桃子，在H小时内吃完的最少速度

---
+++ hugh/onon/exam/exam_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --#
+# @Time : 2019/12/28 13:19 
+# @Author : Aries 
+# @Site :  
+# @File : exam_test.py 
+# @Software: PyCharm
+import sys
+
+if __name__ == '__main__':
+    while True:
+        line = sys.stdin.readline().strip()
+        if line == '':
+            break
+        try:
+            data = list(map(int, line.split(' ')))
+            H = data[-1]
+            tao = data[:-1]
+            N = len(tao)
+            if N > H or N == 0 or (0 in tao):
+                print(-1)
+            elif N == H:
+                print(max(tao))
+            else:
+                per = int(H / N)
+                k = max(int(min(tao) / per), 1)
+                while k <= max(tao):
+                    hours = 0
+                    res = True
+                    for val in tao:
+                        if val % k == 0:
+                            hours += int(val / k)
+                        else:
+                            hours += int(val /  k) + 1
+                        if hours > H:
+                            res = False
+                            break
+                    if res:
+                        break
+                    else:
+                        k += 1
+                print(k)
+        except:
+            print(-1)
+            break
