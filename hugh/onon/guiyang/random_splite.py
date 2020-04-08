572df8382c53ab0910fa126538d83fa7d3d05b84
hughChen <shuliyuanshi@163.com>
2020-04-08 14:54:59 +0800

    [add] 模型的随机切分

--- hugh/onon/guiyang/random_splite.py
+++ hugh/onon/guiyang/random_splite.py
@@ -17,9 +17,11 @@ if __name__ == '__main__':
     '''
     切分测试
     '''
-    df = pd.read_csv(r'C:\Users\BBD\Desktop\test\tmp\Cshl0V1SGLeAb6opAABKW103UTU063.csv')
+    df = pd.read_csv(r'C:\Users\BBD\Desktop\test\tmp\test_data_classification.csv')
     print(df)
+    stratify = 'Disbursed'
+    stratify_df = df[stratify] if stratify is not None and stratify != '' else None
 
-    train_df, test_df = model_selection.train_test_split(df, stratify=None, random_state=9, train_size=0.3)
+    train_df, test_df = model_selection.train_test_split(df, stratify=stratify_df, random_state=9, train_size=0.3)
     print(train_df)
     print(test_df)
