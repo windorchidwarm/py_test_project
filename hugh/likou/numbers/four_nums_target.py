17c6c53757dc6fcb583f6c097f81b3cc61c5f829
hughChen <shuliyuanshi@163.com>
2020-04-14 12:42:20 +0800

    [add] dfs深度搜索 [fixed] 部分剪纸

--- hugh/likou/numbers/four_nums_target.py
+++ hugh/likou/numbers/four_nums_target.py
@@ -28,14 +28,15 @@ def four_nums_target(nums: List[int], target: int) -> List[List[int]]:
     lst = []
     i = 0
     while i < n - 3:
+        if nums[i] * 4 > target: break
         i_target = target - nums[i]
         j = i + 1
         while j < n - 2:
-            if nums[j] > i_target and nums[j] > 0: break
+            if nums[j] * 3 > i_target: break
             first = j + 1
             last = n - 1
             while first < last:
-                if first >= last: break
+                if first >= last  or nums[last] * 4 < target: break
                 result = nums[j] + nums[first] + nums[last]
                 if result == i_target:
                     lst.append([nums[i], nums[j], nums[first], nums[last]])
@@ -53,8 +54,52 @@ def four_nums_target(nums: List[int], target: int) -> List[List[int]]:
         i += 1
     return lst
 
+
+def n_sum(nums, target, n):
+    def dfs(pos: int, cur: List[int], n: int, target: int):
+        if n == 2:
+            j = pos
+            k = len(nums) - 1
+            while j < k:
+                sum = nums[j] + nums[k]
+                if sum < target:
+                    j += 1
+                elif sum > target:
+                    k -= 1
+                else:
+                    solution = cur[:] + [nums[j], nums[k]]
+                    ans.append(solution)
+                    while j < k and nums[j] == nums[j + 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k - 1]:
+                        k -= 1
+                    j += 1
+                    k -= 1
+            return
+        i = pos
+        while i < len(nums) - n + 1:
+            # 剪枝的一种情况
+            if nums[i] * n > target or nums[-1] * n < target:
+                break
+            # 排除重复数字
+            if i > pos and nums[i] == nums[i - 1]:
+                i += 1
+                continue
+            cur.append(nums[i])
+            dfs(i + 1, cur, n - 1, target - nums[i])  # 回溯
+            cur.pop()
+            i += 1
+
+    ans = []
+    nums.sort()
+    dfs(0, [], n, target)
+    return ans
+
+
 if __name__ == '__main__':
     nums =  [1,-2,-5,-4,-3,3,3,5]
     target = -11
     l = four_nums_target(nums, target)
     print(l)
+    ll = n_sum(nums, target, 4)
+    print(ll)
