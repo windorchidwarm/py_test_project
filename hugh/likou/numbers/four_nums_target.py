83e8c20abca1bd8aab59c546fa5dce512df9c38c
hughChen <shuliyuanshi@163.com>
2020-04-14 11:30:00 +0800

    [add] python typing 返回类型指定

--- hugh/likou/numbers/four_nums_target.py
+++ hugh/likou/numbers/four_nums_target.py
@@ -6,7 +6,9 @@
 # @File : four_nums_target.py 
 # @Software: PyCharm
 
-def four_nums_target(nums, target):
+from typing import List
+
+def four_nums_target(nums: List[int], target: int) -> List[List[int]]:
     '''
     给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
 
