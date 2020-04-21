a996880ef7a1835b8a5f672114476c57fa2c8802
hughChen <shuliyuanshi@163.com>
2020-04-21 10:43:19 +0800

    [add] 合并K个有序序列 优先队列方法

--- hugh/likou/node/node_operate.py
+++ hugh/likou/node/node_operate.py
@@ -7,6 +7,8 @@
 # @Software: PyCharm
 
 from hugh.likou.node.list_node import ListNode
+from typing import List
+from queue import PriorityQueue
 
 
 def removeNthFromEnd(head: ListNode, n: int) -> ListNode:
@@ -26,6 +28,40 @@ def removeNthFromEnd(head: ListNode, n: int) -> ListNode:
     return head
 
 
+def mergeKLists(lists: List[ListNode]) -> ListNode:
+    '''
+    合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
+    输入:
+    [
+      1->4->5,
+      1->3->4,
+      2->6
+    ]
+    输出: 1->1->2->3->4->4->5->6
+    :param self:
+    :param lists:
+    :return:
+    '''
+    if lists is None or len(lists) < 1:return
+
+    head = point = ListNode(0)
+    q = PriorityQueue()
+    n = 0
+    for l in lists:
+        if l:
+            q.put((l.val, n, l))
+            n += 1
+    while not q.empty():
+        val, n, node = q.get()
+        point.next = ListNode(val)
+        point = point.next
+        node = node.next
+        if node:
+            q.put((node.val, n, node))
+            n += 1
+    return head.next
+
+
 
 if __name__ == '__main__':
     '''
@@ -40,3 +76,27 @@ if __name__ == '__main__':
         next = node
     print(head)
     removeNthFromEnd(head, 2)
+
+    lists = []
+    head = ListNode(1)
+    next = head
+    for i in (4, 6):
+        node = ListNode(i)
+        next.next = node
+        next = node
+    lists.append(head)
+    head = ListNode(2)
+    next = head
+    for i in (4, 5):
+        node = ListNode(i)
+        next.next = node
+        next = node
+    lists.append(head)
+    head = ListNode(1)
+    next = head
+    for i in (3, 4):
+        node = ListNode(i)
+        next.next = node
+        next = node
+    lists.append(head)
+    mergeKLists(lists)
