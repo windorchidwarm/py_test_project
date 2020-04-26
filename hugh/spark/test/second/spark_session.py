d925b80f68c013255dc0279824eb037722708c7b
hughChen <shuliyuanshi@163.com>
2020-04-26 17:25:35 +0800

    [add] spark test

--- hugh/spark/test/second/spark_session.py
+++ hugh/spark/test/second/spark_session.py
@@ -7,6 +7,10 @@
 
 from pyspark import SparkContext,SparkConf
 from hugh.spark.test.second import spark_config
+from pyspark.sql import SparkSession
+import json
+from pyspark.sql.types import Row
+from pyspark.rdd import PipelinedRDD
 
 from operator import add
 import re
@@ -15,6 +19,17 @@ def get_session():
     conf = SparkConf().setMaster('local').setAppName('first')
     sc = SparkContext(conf=conf)
     return sc
+
+def get_spark():
+    conf = SparkConf().setMaster('local').setAppName('first')
+
+    spark = SparkSession.builder \
+        .appName('first') \
+        .config(conf=conf) \
+        .enableHiveSupport() \
+        .getOrCreate()
+    return spark
+
 
 def not_contain_chinese(data):
     '''
@@ -50,12 +65,73 @@ def wind_some2(sc):
     test_rdd = sc.textFile(path)
     test_rdd.map(test_opt).collect()
 
+    csv_path = r'C:\Users\BBD\Desktop\test\tmp\Cshl0V1SGTmALptAAAAi230xNmU755.csv'
+    test_rdd_csv = sc.textFile(csv_path)
+    print('---------------')
+    print(type(test_rdd_csv))
+    df = sc.textFile
+    print(df)
+
+
+def groupby_map(row):
+    print(type(row))
+    row_dict = row.asDict()
+    print(row_dict)
+    print(type(row_dict))
+
+    return row
+
+    # value = json.loads(data)
+    # print(value)
+    # if '编号' in value.keys():
+    #     return (
+    #         value['编号'],
+    #         data.value)
+    # else:
+    #     return ('null', 'null')
+
+
+
+
+
+def spark_some(spark):
+    def df_map(data):
+        print(data)
+        (key1, key2 ), data2 = data
+        print(type(data))
+        print(key1, key2, data2)
+        data3 = {}
+        data3['kye1'] = key1
+        data3['key2'] = key2
+        for mm in data2:
+            print(mm)
+        print(type(data2))
+        return data3
+    csv_path = r'C:\Users\BBD\Desktop\test\tmp\Cshl0V1SGTmALptAAAAi230xNmU755.csv'
+    df = spark.read.csv(csv_path, header=True, inferSchema=True, sep='\u0001')
+    print(df)
+    print(type(df.rdd))
+    df_new = df.rdd.map(
+        lambda r: ((r['商场名称'], r['商场ID']), r.asDict())
+    ).groupByKey(
+    ).map(
+        df_map
+    )
+    print(df_new)
+    print(type(df_new))
+    df_new_df = df_new.toDF()
+    print(df_new_df.schema)
+    print(df_new_df)
+
 
 if __name__ == '__main__':
     '''
     spark session 相关的测试
     '''
-    sc = get_session()
-    print(sc)
-    wind_some2(sc)
+    # sc = get_session()
+    # print(sc)
+    # wind_some2(sc)
+    spark = get_spark()
+    print(spark)
+    spark_some(spark)
     # wind_some(sc)
