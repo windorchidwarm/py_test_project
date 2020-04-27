9ce859ed4903c7481fe03b508ff6e6b2e0049ba6
hughChen <shuliyuanshi@163.com>
2020-04-27 09:43:47 +0800

    [add] tree node class

---
+++ hugh/likou/node/tree_node.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --#
+# @Time : 2020/4/22 9:16 
+# @Author : Aries 
+# @Site :  
+# @File : tree_node.py 
+# @Software: PyCharm
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
