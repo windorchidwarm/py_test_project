db2f56f1f4a129987cc257fd3c674283d77dd83b
hughChen <shuliyuanshi@163.com>
2020-05-04 08:05:41 +0800

    [add] 获取下一个排列

--- hugh/likou/numbers/num_sort.py
+++ hugh/likou/numbers/num_sort.py
@@ -61,5 +61,50 @@ def reversePairs2(nums: List[int]) -> int:
     return ans
 
 
+
+def swap(nums, i, j):
+    tmp = nums[i]
+    nums[i] = nums[j]
+    nums[j] = tmp
+
+
+def next_reverse(nums, start):
+    i = start
+    j = len(nums) - 1
+    while i < j:
+        swap(nums, i, j)
+        i += 1
+        j -= 1
+
+
+def nextPermutation(nums: List[int]) -> None:
+    '''
+    实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+    如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+    必须原地修改，只允许使用额外常数空间。
+    1,2,3 → 1,3,2
+    3,2,1 → 1,2,3
+    1,1,5 → 1,5,1
+    :param self:
+    :param nums:
+    :return:
+    '''
+    i = len(nums) - 2
+    while i >= 0 and nums[i + 1] <= nums[i]:
+        i -= 1
+    if i >= 0:
+        j = len(nums) - 1
+        while j >= 0 and nums[j] <= nums[i]:
+            j -= 1
+        swap(nums, i, j)
+    print(i)
+    next_reverse(nums, i + 1)
+
+
+
 if __name__ == '__main__':
     print(reversePairs2([7, 5, 6, 4]))
+    nums = [3, 2, 1]
+    nums = [1, 2, 3]
+    nextPermutation(nums)
+    print(nums)
