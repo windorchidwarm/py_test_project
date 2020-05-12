7095437f3f438042905fb18296c4f61bb9b53044
hughChen <shuliyuanshi@163.com>
2020-05-12 13:42:17 +0800

    [add] class list test

---
+++ hugh/class_game/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File  : __init__.py.py
+# Author: BBD
+# Date  : 2020/5/7
---
+++ hugh/class_game/test_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File  : test_list.py
+# Author: BBD
+# Date  : 2020/5/7
+
+if __name__ == '__main__':
+    print([].__sizeof__()) # size 40
+    lst = [1, 2, 4]
+    lst.insert(1, 6) # 插入超过限制就是最后一列
+    print(lst)
+    lst.pop()
+    print(lst.__sizeof__()) #
+    print(lst)
