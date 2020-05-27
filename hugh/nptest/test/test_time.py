9603003652c2725adaecdac72e81aabccbaa1781
hughChen <shuliyuanshi@163.com>
2020-05-27 10:41:08 +0800

    [add] timeit 一个性能的测试函数

---
+++ hugh/nptest/test/test_time.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File  : test_time.py
+# Author: BBD
+# Date  : 2020/5/27
+
+import timeit
+
+
+if __name__ == '__main__':
+    '''
+    测试性能
+    '''
+    print(timeit.__all__)
+    print(timeit._globals)
+    print(timeit.timeit('print(i for i in range(199))', number=1000))
