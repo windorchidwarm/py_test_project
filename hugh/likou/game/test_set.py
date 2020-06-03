2dfd71e37993727c9bbc76bc29f9de330e15cc16
hughChen <shuliyuanshi@163.com>
2020-06-03 17:57:53 +0800

    [add] 删除链表中倒数第N个节点

--- hugh/likou/game/test_set.py
+++ hugh/likou/game/test_set.py
@@ -15,3 +15,12 @@ if __name__ == '__main__':
     print(lset & rset)
     print(lset - rset)
     print(lset <= rset)
+
+    print(lset.union(rset))
+    print(lset.intersection(rset))
+    print(lset.difference(rset))
+    print(lset.add(4))
+    print(lset)
+    print(rset.issubset(lset))
+    # print(lset.intersection_update(rset))
+    # print(lset)
