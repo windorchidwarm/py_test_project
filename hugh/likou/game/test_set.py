109c6021b58d5e3f6fc77fe29cdd6e9f5e6c4933
hughChen <shuliyuanshi@163.com>
2020-06-03 17:53:47 +0800

    [add] set操作

---
+++ hugh/likou/game/test_set.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File  : test_set.py
+# Author: BBD
+# Date  : 2020/6/3
+
+
+if __name__ == '__main__':
+    '''
+    '''
+
+    lset = {1 ,2 ,3}
+    rset = {2,3,4}
+    print(lset | rset)
+    print(lset & rset)
+    print(lset - rset)
+    print(lset <= rset)
