e126aa4b360ffc4c587430a44f65c1a0c06a189c
hughChen <shuliyuanshi@163.com>
2020-06-23 11:06:55 +0800

    [add] 最小二乘法 实现 矩阵

---
+++ hugh/base_calc/math_calc/test_ols.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File  : test_ols.py
+# Author: hugh
+# Date  : 2020/6/23
+
+from numpy.linalg import inv # 矩阵求逆
+from numpy import dot # 矩阵点乘
+from numpy import mat # 二维矩阵
+
+if __name__ == '__main__':
+    '''
+    '''
+    X = mat([1, 2, 3]).reshape(3, 1)
+    Y = mat([5, 10, 15]).reshape(3, 1)
+    x_t = X.T
+    x_x = dot(x_t, X)
+    x_x_inv = inv(x_x)
+    inv_x = dot(x_x_inv, x_t)
+    res = dot(inv_x, Y)
+
+    print(X)
+    print(Y)
+    print(x_t)
+    print(x_x)
+    print(x_x_inv)
+    print(inv_x)
+    print(res)
