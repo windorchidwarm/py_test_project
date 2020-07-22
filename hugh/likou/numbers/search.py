2d06b6141796f40305f9f9c19dd77904b287736c
hughChen <shuliyuanshi@163.com>
2020-07-22 10:34:56 +0800

    [add] 旋转数组最小值

--- hugh/likou/numbers/search.py
+++ hugh/likou/numbers/search.py
@@ -80,6 +80,33 @@ def searchInsert(nums: List[int], target: int) -> int:
     nums.insert(l + 1, target)
     return l + 1
 
+
+def minArray(numbers: List[int]) -> int:
+    '''
+    把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，
+    输出旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。
+    :param numbers:
+    :return:
+    '''
+    if len(numbers) == 0: return
+    if len(numbers) == 1: return numbers[0]
+    l = 0
+    r = len(numbers) - 1
+    if numbers[0] < numbers[-1]:
+        return numbers[0]
+    while l < r:
+        mid = (l + r) // 2
+        if numbers[l] > numbers[mid]:
+            r = mid
+        elif numbers[mid] > numbers[r]:
+            l = mid + 1
+        else:
+            r -= 1
+    return numbers[r]
+
+
+
 if __name__ == '__main__':
     # print(search([3,1], 1))
-    print(searchInsert([1,3,5,6], 7))
+    # print(searchInsert([1,3,5,6], 7))
+    print(minArray([3,1,1]))
