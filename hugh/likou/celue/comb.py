157e69d6ca7e71ad293c451520506b1dbb06de0d
hughChen <shuliyuanshi@163.com>
2020-07-23 10:51:11 +0800

    [add] 给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。     candidates 中的每个数字在每个组合中只能使用一次。

--- hugh/likou/celue/comb.py
+++ hugh/likou/celue/comb.py
@@ -33,7 +33,7 @@ def combinationSum(candidates: List[int], target: int) -> List[List[int]]:
     ans.reverse()
     return ans
 
-def combinationSum2(candidates: List[int], target: int) -> List[List[int]]:
+def combinationSum_2(candidates: List[int], target: int) -> List[List[int]]:
     '''
     用背包问题解
     :param candidates:
@@ -47,7 +47,30 @@ def combinationSum2(candidates: List[int], target: int) -> List[List[int]]:
             dp[i] += [res + [candidate] for res in dp[i - candidate]]
     return dp[-1]
 
+def combinationSum2(candidates: List[int], target: int) -> List[List[int]]:
+    '''
+    给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+    candidates 中的每个数字在每个组合中只能使用一次。
+    :param candidates:
+    :param target:
+    :return:
+    '''
+    dp = [[[]] if j == 0 else [] for j in range(target + 1)]
+    candidates.sort()
+    for candidate in candidates:
+        for i in range(target, candidate - 1, -1):
+            print(dp)
+            for res in dp[i - candidate]:
+                ret = res + [candidate]
+                if ret not in dp[i]:
+                    dp[i].append(ret)
+            # dp[i] += [res + [candidate] for res in dp[i - candidate]]
+    ans = dp[-1]
+    ans.sort()
+    return ans
+
 if __name__ == '__main__':
     '''
     '''
-    print(combinationSum2([2,3,6,7], 7))
+    # print(combinationSum_2([2,3,6,7], 7))
+    print(combinationSum2([2,5,2,1,2], 5))
