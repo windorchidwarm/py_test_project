bdf8fe26b2d82b205468dcbd5bf6e5abac73ec66
hughChen <shuliyuanshi@163.com>
2020-08-18 13:59:15 +0800

    [add] 有序链表转平衡二叉搜索树

--- hugh/likou/node/bst.py
+++ hugh/likou/node/bst.py
@@ -5,6 +5,8 @@
 # Date  : 2020-05-05
 
 from hugh.likou.node.tree_node import TreeNode
+from hugh.likou.node.list_node import ListNode
+from typing import List
 import numpy as np
 
 def isValidBST(root: TreeNode) -> bool:
@@ -31,6 +33,44 @@ def isValidBST(root: TreeNode) -> bool:
         return True
 
     return helper(root)
+
+def sortedListToBST(self, head: ListNode) -> TreeNode:
+    '''
+    给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+    本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+    示例:
+    给定的有序链表： [-10, -3, 0, 5, 9],
+
+    一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+
+          0
+         / \
+       -3   9
+       /   /
+     -10  5
+    :param self:
+    :param head:
+    :return:
+    '''
+    def getLength(head:ListNode) -> int:
+        ret = 0
+        while head:
+            ret += 1
+            head = head.next
+        return ret
+
+    def buildTree(left:int, right:int) -> TreeNode:
+        if left > right: return None
+        mid = (right + left + 1) // 2
+        root = TreeNode()
+        root.left = buildTree(left, mid - 1)
+        nonlocal head
+        root.val = head.val
+        head = head.next
+        root.right = buildTree(mid + 1, right)
+        return root
+    length = getLength(head)
+    return buildTree(0, length - 1)
 
 if __name__ == '__main__':
     isValidBST(None)
