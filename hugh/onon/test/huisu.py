7636300b1e686f6a88082c3bfe2841bf27114dfb
hughChen <shuliyuanshi@163.com>
2020-08-18 14:15:42 +0800

    [add] 全排列

--- hugh/onon/test/huisu.py
+++ hugh/onon/test/huisu.py
@@ -5,6 +5,7 @@
 # Date  : 2019-11-21
 
 import copy
+from typing import List
 
 class Solution(object):
     def permute(self, nums):
@@ -30,7 +31,30 @@ class Solution(object):
         print(tmpList)
         return tmpList
 
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def dfs(nums, size, depth, path, used, res):
+            if depth == size:
+                res.append(path.copy())
+                return
+            for i in range(size):
+                if not used[i]:
+                    if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
+                        continue
+                    used[i] = True
+                    path.append(nums[i])
+                    dfs(nums, size, depth + 1, path, used, res)
+                    used[i] = False
+                    path.pop()
+        size = len(nums)
+        if size == 0: return []
+        nums.sort()
+        used = [False] * size
+        res = []
+        dfs(nums, size, 0, [], used, res)
+        return res
+
 if __name__ == '__main__':
     nums = [1, 2, 3, 4]
     test = Solution()
     test.permute(nums)
+    test.permuteUnique([1,1,2])
